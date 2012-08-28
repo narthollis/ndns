1037b00441be87d92b7b535923c5663fa138b022
Nicholas Steicke <nicholas.steicke@narthollis.net>
2012-08-28 16:42:18 +0930

    fixed missnamed variable - added required getNameservers method

--- providers/null.py
+++ providers/null.py
@@ -7,16 +7,19 @@ It is not useful for actual work
 class NullProvider:
     
     def __init__(self):
-        self.providers = [] 
+        self.filters = [] 
     
     def getZones(self, clientaddress):
+        return []
+    
+    def getNameservers(self, zone):
         return []
     
     def getResponse(self, query, zone, qtype, qclass, clientaddress):
         return (None,None)
     
     def getFilters(self):
-        return []
+        return self.filters
     
     def addFilter(self, f):
-        self.providers.append(f)
+        self.filters.append(f)
