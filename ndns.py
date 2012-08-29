c3014ef5bde3a8718b0d6bf6989012a715feed74
Nicholas Steicke <nicholas.steicke@narthollis.net>
2012-08-29 16:57:28 +0930

    Now working using pythondns

--- ndns.py
+++ ndns.py
@@ -11,6 +11,9 @@ import queue
 import threading
 import logging
 
+import dns
+import dns.message
+
 import utils
 
 logger = logging.getLogger('DNS')
@@ -25,183 +28,39 @@ class DnsError(Exception):
 class DnsRequestHandler(threading.Thread):
     
     def __init__(self, ndns, request, isUdp, clientaddress):
-        self.request = request
+        self.raw_request = request        
         self.ndns = ndns
         self.isUdp = isUdp
         self.clientaddress = clientaddress
         
         super().__init__()
     
-    def parseRequest(self, request):
-        headerLength = 12
-        header = request[:headerLength]
-        
-        qid, flags, qdcount, _, _, _ = struct.unpack('!HHHHHH', header)
-        qr = (flags >> 15) & 0x1
-        opcode = (flags >> 11) & 0xf
-        rd = (flags >> 8) & 0x1
-                
-        if qr != 0 or opcode != 0 or qdcount == 0:
-            raise DnsError("Invalid query", rcode=1)
-        
-        body = request[headerLength:]
-        labels = []
-        offset = 0
-        
-        while True:
-            label_len, = struct.unpack('!B', body[offset:offset+1])
-            offset += 1
-            if label_len & 0xc0:
-                raise DnsError("Invalid label length %d" % label_len)
-            if label_len == 0:
-                break
-            label = body[offset:offset+label_len]
-            offset += label_len
-            labels.append(label)
-            
-        qtype, qclass= struct.unpack("!HH", body[offset:offset+4])
-        
-        if qclass != 1:
-            raise DnsError("Invalid class: " + qclass)
-        return (qid, labels, qtype, qclass)
-    
-    def buildNameServerResource(self, provider, zone):
-        ns = []
-        ar = []
-        
-        namesInNS = []
-        for name_server, ip, ttl in provider.getNameServers(zone, self.clientaddress):
-            if not name_server in namesInNS:
-                ns.append({
-                       'qtype':2,
-                       'qclass':1,
-                       'ttl':ttl,
-                       'rdata':utils.labels2str(name_server),
-                       'question': zone
-                    })
-                    
-                namesInNS.append(name_server)           
-            
-            qtype = 1
-            if ip.version == 6:
-                qtype = 28            
-            ar.append({'qtype':qtype, 'qclass':1, 'ttl':ttl, 'rdata':ip.packed})
-    
-        return ns, ar
-    
-    def formatResponse(self, qid, question, qtype, qclass, rcode, an_resource_records, ns_resource_records, ar_resource_records):
-        resources = []
-        resources.extend(an_resource_records)
-        num_an_resources = len(an_resource_records)
-        num_ns_resources = num_ar_resources = 0
-        if rcode == 0:
-            resources.extend(ns_resource_records)
-            resources.extend(ar_resource_records)
-            num_ns_resources = len(ns_resource_records)
-            num_ar_resources = len(ar_resource_records)
-        pkt = self.formatHeader(qid, rcode, num_an_resources, num_ns_resources, num_ar_resources)
-        pkt += self.formatQuestion(question, qtype, qclass)
-        for resource in resources:
-            if 'question' in resource:
-                pkt += self.formatResource(resource, resource['question'])
-            else:
-                pkt += self.formatResource(resource, question)
-        return pkt
-    
-    def formatHeader(self, qid, rcode, ancount, nscount, arcount):
-        flags = 0
-        flags |= (1 << 15)
-        flags |= (1 << 10)
-        flags |= (rcode & 0xf)
-        hdr = struct.pack("!HHHHHH", qid, flags, 1, ancount, nscount, arcount)
-        return hdr
-    
-    def formatQuestion(self, question, qtype, qclass):
-        q = utils.labels2str(question)
-        q += struct.pack("!HH", qtype, qclass)
-        return q
-    
-    def formatResource(self, resource, question):
-        r = b''
-        r += utils.labels2str(question)
-        r += struct.pack("!HHIH", resource['qtype'], resource['qclass'], resource['ttl'], len(resource['rdata']))
-        r += resource['rdata']
-        return r
     
     def run(self):
-        qid = question = qtype = qclass = rcode = None
-        an_resource_records = ns_resource_records = ar_resource_records = []
-        
-        response = None
-        
-        try:
-            (qid, question, qtype, qclass) = self.parseRequest(self.request)
-            
-            question = list(map(lambda x: x.lower(), question))
-            
-            found = False
-            
-            for provider in self.ndns.getProviders():
-                for zone in provider.getZones(self.clientaddress):
-                    if question[1:] == zone:
-                        query = question[0]
-                    elif question == zone:
-                        query = ''
-                    elif question[-len(zone):] == zone:
-                        query = question[:-len(zone)]
-                    else:
-                        continue
-            
-                    ns_resource_records, IGNORE_ar_resource_records = self.buildNameServerResource(provider, zone)
-                    del IGNORE_ar_resource_records
-                    # Copy the NS data for later.
-                    original_ns_resource_records = ns_resource_records
-            
-                    rcode, an_resource_records = provider.getResponse(query, zone, qtype, qclass, self.clientaddress)
+        request = dns.message.from_wire(self.raw_request)
+        response = dns.message.make_response(request)
+
+        found = False
+        for provider in self.ndns.getProviders():
+            for zone in provider.getZones(self.clientaddress):
+                if request.question[0].name.is_subdomain(zone):
+                    resp = provider.getResponse(request, self.clientaddress)
                     
-                    logger.debug("Got from get_response(): %s / %s" % (rcode, an_resource_records))
+                    if resp is not None:
+                        response = resp
+                        found = True
+                        break
                     
-                    if rcode == 0:
-                        for f in provider.getFilters():
-                            logger.debug("Running filter %s" % (f,))
-                            an_resource_records, ns_resource_records = f.filter(query, zone, qtype, qclass, self.clientaddress, an_resource_records, ns_resource_records)
-                            logger.debug("Filter returned %s/%s" % (an_resource_records, ns_resource_records))
-                    
-                    # If there's no error and no responses, wipe NS information
-                    # (or, if it's an NS query, move it to the answer section)
-                    # If we don't do this, it'd be considered a referral instead of an answer...
-                    if rcode == 0 and len(an_resource_records) == 0 and ns_resource_records == original_ns_resource_records:
-                        if qtype == 2:
-                            an_resource_records = ns_resource_records
-                        ns_resource_records = []
-                    
-                    logger.debug("About to send back:\nAN: %s\nNS: %s\nAR: %s\n" % (an_resource_records, ns_resource_records, ar_resource_records))
-                    response = self.formatResponse(qid, question, qtype, qclass, rcode, an_resource_records, ns_resource_records, ar_resource_records)
-                    
-                    found = True
-                    break
-                
-                if found:
-                    break
-                
-            if not found:
-                raise DnsError("query is not for our domain: %s" % ((b".".join(question)).decode('UTF-8'),), 3)
-                    
-        except DnsError as e:
-            if qid:
-                if e.rcode == None:
-                    e.rcode = 2
-                
-                response = self.formatResponse(qid, question, qtype, qclass, e.rcode, [], [], [])
-                
-                logger.error(e)
-            else:
-                pass
-        
+            if found:
+                break
+    
+        if not found:
+            response.set_rcode(dns.rcode.NXDOMAIN)
+    
         if self.isUdp:
-            self.ndns.udpOut.put((response, self.clientaddress))
+            self.ndns.udpOut.put((response.to_wire(), self.clientaddress))
         else:
-            self.ndns.tcpOut.put((response, self.clientaddress))
+            self.ndns.tcpOut.put((response.to_wire(), self.clientaddress))
             
 class Ndns:
     '''
