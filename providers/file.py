6015f21c680d76415345a80e46f460a6257e61fc
Nicholas Steicke <nicholas.steicke@narthollis.net>
2012-09-03 16:56:12 +0930

    Wildcard lookups and ANY responses

--- providers/file.py
+++ providers/file.py
@@ -2,11 +2,11 @@
 import dns.name
 import dns.zone
 import dns.message
+import dns.rdatatype
 
 """
 This is a very basic dns provider that reads a zone file and
-loads it into memory. The initial implemtation only supports
-SOA, NS, A, AAAA, CNAME, MX and TXT records.
+loads it into memory.
 """
 
 
@@ -14,38 +14,69 @@ class FileProvider:
     def __init__(self, file, zone):
         print (file, zone)
         self.zone = dns.name.from_text(zone)
-        self.data = dns.zone.from_file(file, zone)
+        self.data = dns.zone.from_file(
+            file,
+            origin=zone,
+            relativize=False
+        )
 
         self.filters = []
 
     def getZones(self, clientaddress):
-        print(self.zone)
         return [self.zone]
+
+    def getRdatasetWildcardRecursion(self, name, rdtype, first=True):
+        try:
+            rdataset = self.data.find_rdataset(
+                name,
+                rdtype
+            )
+        except KeyError as e:
+            if name.is_wild():
+                name = name.parent()
+            name.relativize(self.zone)
+            if not first:
+                name = name.parent()
+            name = dns.name.Name(['*']).concatenate(name)
+
+            rdataset = self.getRdatasetWildcardRecursion(name, rdtype, False)
+
+        return rdataset
 
     def getResponse(self, request, clientaddress):
         response = dns.message.make_response(request)
 
         for question in response.question:
-            rdataset = self.data.find_rdataset(question.name, question.rdtype)
-            rrset = response.find_rrset(
-                response.answer,
-                question.name,
-                rdataset.rdclass,
-                rdataset.rdtype,
-                rdataset.covers,
-                None,
-                True
-            )
+            rdtypes = [question.rdtype]
 
-            import types
-            for item in rdataset:
-                print(type(item))
-                for key in dir(item):
-                    i = item.__getattribute__(key)
-                    if isinstance(type(i), types.MethodType):
-                        continue
-                    print (key, ':', i)
-                rrset.add(item)
+            if question.rdtype == dns.rdatatype.ANY:
+                rdtypes = dns.rdatatype._by_value.keys()
+
+            for rdtype in rdtypes:
+                try:
+                    rdataset = self.getRdatasetWildcardRecursion(
+                        question.name,
+                        rdtype
+                    )
+
+                    rrset = response.find_rrset(
+                        response.answer,
+                        question.name,
+                        rdataset.rdclass,
+                        rdataset.rdtype,
+                        rdataset.covers,
+                        None,
+                        True
+                    )
+
+                    for item in rdataset:
+                        rrset.add(item, rdataset.ttl)
+
+                except (KeyError, dns.name.NoParent) as e:
+                    pass
+
+        if len(response.answer) <= 1:
+            response.set_rcode(dns.rcode.NXDOMAIN)
 
         return response
 
